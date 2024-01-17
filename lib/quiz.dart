bd469b282050a198e999ddf135997791153ffa7b
Dima <dimkuh2020@gmail.com>
2024-01-17 22:22:10 +0200

    initState()

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:udemy_quiz_app/questions_screen.dart';
 import 'package:udemy_quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -12,6 +12,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() { // сменя екрана для кнопки
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
    return MaterialApp(
@@ -27,7 +41,7 @@ class _QuizState extends State<Quiz> {
                 end: Alignment.bottomRight,
               ),
             ),
-            child: const StartScreen()),
+            child: activeScreen),
       ),
     );    
   }
