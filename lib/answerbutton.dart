b1ecf60cf6dc99e8e0052876b20c14dfa8bb165b
Dima <dimkuh2020@gmail.com>
2024-01-22 23:54:01 +0200

    button style

---
+++ lib/answerbutton.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  AnswerButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+       child: const Text('Answer 1')
+    );
+  }
+}
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,6 +11,29 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Text('QuestionsScreen');
+    return SizedBox(
+      width: double.infinity, //на всю ширину екрана
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'The question...', 
+            style: TextStyle(
+              color: Colors.amber
+            ),
+          ),
+          const SizedBox(height: 30),
+          
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Answer 2')
+          ),
+          ElevatedButton(
+            onPressed: () {}, 
+            child: const Text('Answer 3')
+          ),
+        ],
+      ),
+    );
   }
 }
